fb39c48c64d7e681b481cb58dd10fdab45e8ab8a
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-01-08 21:26:17 -0200

    feat: added _app entrypoint to inject antd scripts

---
+++ pages/_app.js
@@ -0,0 +1,26 @@
+import App, { Container } from 'next/app'
+
+// import global antd styles
+import 'antd/dist/antd.min.css'
+
+export default class MyApp extends App {
+  // static async getInitialProps ({ Component, router, ctx }) {
+  //   let pageProps = {}
+
+  //   if (Component.getInitialProps) {
+  //     pageProps = await Component.getInitialProps(ctx)
+  //   }
+
+  //   return { pageProps }
+  // }
+
+  render () {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    )
+  }
+}
