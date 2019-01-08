2de1a91a63efce7e927cdd714bc498d747b633d3
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-01-08 21:01:34 -0200

    refactor: fixed linting warning

--- pages/about.js
+++ pages/about.js
@@ -1,32 +1,30 @@
-import { Component } from "react";
-import Link from "next/link";
-import Header from "../components/header";
+import { Component } from 'react'
+import Link from 'next/link'
+import Header from '../components/header'
 
 class AboutPage extends Component {
-  static getInitialProps() {
-    const isServer = typeof window === "undefined";
-    return { isServer };
+  static getInitialProps () {
+    const isServer = typeof window === 'undefined'
+    return { isServer }
   }
 
-  render() {
+  render () {
     return (
       <main>
         <Header />
         <section>
           <p>
-            This is another page of the SSR example, you accessed it{" "}
-            <strong>{this.props.isServer ? "server" : "client"} side</strong>.
+            This is another page of the SSR example, you accessed it{' '}
+            <strong>{this.props.isServer ? 'server' : 'client'} side</strong>.
           </p>
-          <p>
-            You can reload to see how the page change.
-          </p>
-          <Link href="/">
+          <p>You can reload to see how the page change.</p>
+          <Link href='/'>
             <a>Go to Home</a>
           </Link>
         </section>
       </main>
-    );
+    )
   }
 }
 
-export default AboutPage;
+export default AboutPage
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import Link from "next/link";
-import Header from "../components/header";
+import Link from 'next/link'
+import Header from '../components/header'
 
 export default () => (
   <main>
     <Header />
     <section>
-      <Link href="/about">
+      <Link href='/about'>
         <a>Go to About Me</a>
       </Link>
     </section>
   </main>
-);
+)
