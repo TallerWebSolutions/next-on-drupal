acee8f956bf4ddb7a55a7f04773d48bfcf8455b5
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-01-10 23:16:04 -0200

    feat: added styled-components

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import Document from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  static async getInitialProps ({ renderPage, ...ctx }) {
+    const sheet = new ServerStyleSheet()
+
+    // execute parent Document's getInitialProps with a custom renderPage
+    const { styles, ...initialProps } = await Document.getInitialProps({
+      renderPage: () =>
+        renderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App { ...props } />)
+        }),
+      ...ctx
+    })
+
+    return {
+      ...initialProps,
+      styles: [...styles, ...sheet.getStyleElement()]
+    }
+  }
+}
