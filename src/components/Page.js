e942cd11849a7f1d9f41856ae7208c0172c06126
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-01-11 10:50:58 -0200

    feat: added base Page component

---
+++ src/components/Page.js
@@ -0,0 +1,32 @@
+/**
+ * Component used for mostly all pages.
+ *
+ * Useful to unifying general meta-data/layout setup.
+ */
+
+import { Fragment } from 'react'
+import { string, node } from 'prop-types'
+import Head from 'next/head'
+
+const baseTitle = 'TallerDashboard'
+
+const Page = ({ title, children }) => (
+  <Fragment>
+    <Head>
+      <title>{ title.replace('%base', baseTitle) }</title>
+    </Head>
+
+    { children }
+  </Fragment>
+)
+
+Page.propTypes = {
+  children: node.isRequired,
+  title: string
+}
+
+Page.defaultProps = {
+  title: '%base'
+}
+
+export default Page
