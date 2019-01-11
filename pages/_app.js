5bd045ab3e18a1955d836b14c879f5ebd2c6ac87
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-01-11 10:48:34 -0200

    feat: added global styles and initiated styleguide

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import App, { Container } from 'next/app'
+import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
 
 // import global antd styles
 import 'antd/dist/antd.min.css'
+
+import { colors } from '@src/styleguide'
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Alegreya:400,700');
+  @import url('https://fonts.googleapis.com/css?family=Roboto+Slab:400,700');
+
+  div#__next, html, body  {
+    margin: 0;
+    color: ${colors.text};
+    background-color: ${colors.background};
+    font-size: 16px;
+    font-family: 'Alegreya', 'aleo', Georgia, 'Times New Roman', Times,
+      Georgia, 'DejaVu Serif', serif;
+  }
+`
 
 export default class MyApp extends App {
   // static async getInitialProps ({ Component, router, ctx }) {
@@ -19,6 +37,10 @@ export default class MyApp extends App {
 
     return (
       <Container>
+        <GlobalStyle />
+        <Head>
+          <title>Taller Dashboard</title>
+        </Head>
         <Component { ...pageProps } />
       </Container>
     )
---
+++ src/styleguide/colors.js
@@ -0,0 +1,16 @@
+/**
+ * Color library
+ */
+
+// descriptive
+export const black = '#29292F'
+export const grey = '#40454a'
+export const red = '#e9515d'
+export const ice = '#f3f4f4'
+
+// semantic
+export const primary = red
+
+// functional
+export const text = grey
+export const background = ice
