4c82aef09ad54ebe2e7c89299d5f0a27e709d8ab
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-01-17 23:05:24 -0200

    feat: reset pages

--- components/header.js
+++
@@ -1,5 +0,0 @@
-export default () => (
-  <header>
-    <h1>Next.js Example on Now 2.0</h1>
-  </header>
-);
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,1 @@
-import Link from 'next/link'
-import Header from '../components/header'
-
-export default () => (
-  <main>
-    <Header />
-    <section>
-      <Link href='/about'>
-        <a>Go to About Me</a>
-      </Link>
-    </section>
-  </main>
-)
+export default () => 'Hello world!'
