fb79f03df45da306de1112e85b4295a99302e75f
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-01-20 22:26:57 -0200

    chore: added component path resolver

---
+++ .babelrc.js
@@ -0,0 +1,47 @@
+const { existsSync, statSync } = require('fs')
+const { basename, dirname, extname, join, resolve } = require('path')
+const { resolvePath } = require('babel-plugin-module-resolver')
+
+/**
+ * Babel-module-resolve customization to map "/Component" to "/Component/Component.js"
+ * paths when necessary.
+ */
+const resolveComponentPath = (sourcePath, currentFile, opts) => {
+  const defaultPath = resolvePath(sourcePath, currentFile, opts)
+
+  try {
+    const resolved = resolve(dirname(currentFile), defaultPath)
+
+    if (statSync(resolved).isDirectory()) {
+      const ext = extname(sourcePath) || '.js'
+      const index = join(resolved, 'index' + ext)
+      const component = join(resolved, basename(resolved) + ext)
+
+      if (!existsSync(index) && existsSync(component)) {
+        return component
+      }
+    }
+  } catch (err) {
+    // ignore errors, simply return default resolved path.
+  }
+
+  return defaultPath
+}
+
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@source': './src',
+          '@styleguide': './src/styleguide'
+        },
+        resolvePath: resolveComponentPath
+      }
+    ],
+    ['styled-components', { ssr: true, displayName: true, preprocess: false }]
+  ]
+}
