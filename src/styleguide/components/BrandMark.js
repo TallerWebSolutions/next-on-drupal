91fcafd33e17e0771b5825e7fbf66a8de4a22007
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-01-20 23:23:32 -0200

    feat: added BrandMark component

---
+++ src/styleguide/components/BrandMark.js
@@ -0,0 +1,3 @@
+export default props => (
+  <img src='https://static.taller.work/brand-mark.svg' { ...props } />
+)
--- src/styleguide/index.js
+++ src/styleguide/index.js
@@ -1,5 +1,8 @@
 import * as colors from './colors'
+export * from 'antd'
 
+// automate?
 export { default as Logo } from './components/Logo'
+export { default as BrandMark } from './components/BrandMark'
 
 export { colors }
