93c3d9cf45832a301b89e5b40bff975a4bd67a6c
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-01-20 22:28:07 -0200

    fix: wrong path on _app

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import App from 'next/app'
 import { compose } from 'ramda'
 
 import connectGraphQL from '@source/api/provider'
-import connectGlobalStyles from '@styleguide/providers/global-styles'
+import connectGlobalStyles from '@styleguide/provider'
 
 export default compose(
   connectGlobalStyles,
