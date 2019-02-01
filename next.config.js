e366b6b84a5c7490b13e4732d1356f0da4ceca41
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-01-31 23:48:14 -0200

    chore: added next env config setup

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 const withCSS = require('@zeit/next-css')
 
 module.exports = withCSS({
-  target: 'serverless'
+  target: 'serverless',
+  serverRuntimeConfig: {},
+  // available to both client and server-side.
+  publicRuntimeConfig: {
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    GRAPHQL_HOST: process.env.GRAPHQL_HOST
+  }
 })
