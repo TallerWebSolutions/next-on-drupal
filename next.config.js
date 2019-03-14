19bb4eecbf104c5623f1b4ce3e51d3e67af162d8
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-03-14 01:22:27 -0300

    chore: updated next dist directory to be contained aside server bundle

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const withCSS = require('@zeit/next-css')
 
 module.exports = withCSS({
-  target: 'serverless',
+  target: 'server',
+  distDir: 'build/client',
   serverRuntimeConfig: {},
   // available to both client and server-side.
-  publicRuntimeConfig: {
-    NODE_ENV: process.env.NODE_ENV || 'development',
+  env: {
     GRAPHQL_HOST: process.env.GRAPHQL_HOST
   }
 })
