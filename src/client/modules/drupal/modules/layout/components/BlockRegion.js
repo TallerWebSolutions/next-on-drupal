15b8aad06b1548f43595ec7e8ddf3e31aaf2be76
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-03-15 02:12:46 -0300

    feat: added BlockRegion component to render multiple blocks

---
+++ src/client/modules/drupal/modules/layout/components/BlockRegion.js
@@ -0,0 +1,18 @@
+import React, { Fragment, memo } from 'react'
+import { arrayOf, object } from 'prop-types'
+
+import BlockResolverContainer from '@drupal/modules/layout/containers/BlockResolverContainer'
+
+const BlockRegion = ({ blocks }) => (
+  <Fragment>
+    { blocks.map((block, index) => (
+      <BlockResolverContainer key={ block.entityId } block={ block } />
+    )) }
+  </Fragment>
+)
+
+BlockRegion.propTypes = {
+  blocks: arrayOf(object).isRequired
+}
+
+export default memo(BlockRegion)
