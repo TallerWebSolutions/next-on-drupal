d7be8f4a20798dd6d614e1b338684761c1fb2bd2
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-03-15 02:10:56 -0300

    feat: added BlockContentBasic as a proper sample of block content usage with fragmentation

---
+++ src/client/modules/drupal/modules/layout/blocks/BlockContentBasic/BlockContentBasic.js
@@ -0,0 +1,34 @@
+/**
+ * Sample block defitions.
+ */
+
+import React from 'react'
+import { object } from 'prop-types'
+
+import { typenameIs } from '@drupal/modules/layout/lib/util'
+
+const BlockContentBasic = ({ block: { entityLabel, body } }) =>
+  // render nothing if block empty.
+  !entityLabel && !body ? null : (
+    <div>
+      { entityLabel ? <h4>{ entityLabel }</h4> : null }
+      { body ? (
+        <div dangerouslySetInnerHTML={ { __html: body.processed } } />
+      ) : null }
+    </div>
+  )
+
+BlockContentBasic.propTypes = {
+  block: object.isRequired
+}
+
+/**
+ * Predicate for if a block should be rendered by this component or not.
+ *
+ * @param {Object} block A block as loaded from Drupal.
+ *
+ * @return {Boolean}
+ */
+BlockContentBasic.is = typenameIs('BlockContentBasic')
+
+export default BlockContentBasic
--- src/client/modules/drupal/modules/layout/lib/blocks.js
+++ src/client/modules/drupal/modules/layout/lib/blocks.js
@@ -2,11 +2,13 @@
  * Blocks resolution library.
  */
 
+import BlockContentBasic from '@drupal/modules/layout/blocks/BlockContentBasic'
 import BlockNotFound from '@drupal/modules/layout/blocks/BlockNotFound'
 
 // prettier-ignore
 // the blocks registry, in order of precendence.
 const blocks = [
+  BlockContentBasic,
   BlockNotFound
 ]
 
