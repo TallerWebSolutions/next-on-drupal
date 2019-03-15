22b3b784c9a47c4bd98a685b0683c32854728325
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-03-15 02:56:49 -0300

    chore: added webpack bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,30 @@
-const withCSS = require('@zeit/next-css')
+const compose = require('next-compose')
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
+
 const webpack = require('./config/webpack/webpack.next')
 
-module.exports = withCSS({
+// webpack bundle analyzer.
+// @see: https://github.com/zeit/next-plugins/tree/master/packages/next-bundle-analyzer
+const analyzer = [
+  withBundleAnalyzer,
+  {
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/server.html'
+      },
+      browser: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/client.html'
+      }
+    }
+  }
+]
+
+// core NextJS config.
+const core = {
   webpack,
   target: 'server',
   distDir: 'build/client',
@@ -10,4 +33,6 @@ module.exports = withCSS({
   env: {
     GRAPHQL_HOST: process.env.GRAPHQL_HOST
   }
-})
+}
+
+module.exports = compose([analyzer, core])
