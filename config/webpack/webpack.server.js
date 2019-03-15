68ff32cef3179360b32f0bd3a1d8f305156b5e45
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-03-14 23:15:37 -0300

    chore: moved server webpack to config dir

--- config/webpack/webpack.server.js
+++ config/webpack/webpack.server.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const externals = require('webpack-node-externals')
 
+const cwd = process.cwd()
+
 module.exports = {
   target: 'node',
   optimization: { nodeEnv: false },
   externals: [externals()],
-  entry: path.resolve(__dirname, 'src/server/index.js'),
-  output: { path: path.resolve(__dirname, 'build'), filename: 'server.js' },
+  entry: path.resolve(cwd, './src/server/index.js'),
+  output: { path: path.resolve(cwd, './build'), filename: 'server.js' },
 
   module: {
     rules: [
@@ -17,7 +19,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             babelrc: false,
-            presets: ['./config/babel/.babelrc.server.js']
+            presets: [path.resolve(cwd, './config/babel/.babelrc.server.js')]
           }
         }
       },
