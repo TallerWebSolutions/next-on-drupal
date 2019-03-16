b7e04aacebaa8c0d8ae4853369080b9e0002e89e
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-03-16 14:24:45 -0300

    docs: initiated docz setup

---
+++ doczrc.js
@@ -0,0 +1,14 @@
+export default {
+  // theme: 'my-coolest-theme',
+  // themeConfig: {
+  //   colors: {
+  //     primary: 'tomato',
+  //   },
+  // },
+  // modifyBundlerConfig: config => {
+  //   /* do your magic here */
+  // },
+  // plugins: [
+  //   myCoolPlugin()
+  // ]
+}
