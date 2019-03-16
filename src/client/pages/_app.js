9c720396bf6b01d6faa6dfd493a4669dc87d66d5
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-03-16 02:31:09 -0300

    refactor: moved api from @shared to @client

--- src/client/pages/_app.js
+++ src/client/pages/_app.js
@@ -1,7 +1,7 @@
 import App from 'next/app'
 import { compose } from 'ramda'
 
-import connectGraphQL from '@shared/api/provider'
+import connectGraphQL from '@client/api/provider'
 import connectGlobalStyles from '@styleguide/provider'
 
 const hoistInitialProps = (...hocs) => Component => {
