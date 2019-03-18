49f065edd5673068385054c26c5d8c07669dbe25
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-03-18 18:05:15 -0300

    feat: added BlockSiteBranding block

---
+++ src/client/modules/drupal/modules/layout/blocks/BlockSiteBranding.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { object } from 'prop-types'
+import styled from 'styled-components'
+
+import { Logo } from '~styleguide'
+
+const Styled = styled.div`
+  text-align: center;
+
+  small {
+    display: block;
+    font-style: italic;
+
+    img {
+      height: 3em;
+      margin-top: -0.4em;
+      vertical-align: middle;
+    }
+  }
+`
+
+const BlockSiteBranding = ({ block }) => (
+  <Styled>
+    Welcome to the <b>Next on Drupal</b> boilerplate!
+    <small>
+      brought to you by{ ' ' }
+      <a href='https://taller.net.br'>
+        <Logo />
+      </a>
+    </small>
+  </Styled>
+)
+
+BlockSiteBranding.propTypes = {
+  block: object.isRequired
+}
+
+BlockSiteBranding.is = ({ entityId }) => entityId === 'sitebranding'
+
+export default BlockSiteBranding
--- src/client/modules/drupal/modules/layout/lib/blocks.js
+++ src/client/modules/drupal/modules/layout/lib/blocks.js
@@ -2,12 +2,14 @@
  * Blocks resolution library.
  */
 
+import BlockSiteBranding from '~drupal/modules/layout/blocks/BlockSiteBranding'
 import BlockContentBasic from '~drupal/modules/layout/blocks/BlockContentBasic'
 import BlockNotFound from '~drupal/modules/layout/blocks/BlockNotFound'
 
 // prettier-ignore
 // the blocks registry, in order of precendence.
 const blocks = [
+  BlockSiteBranding,
   BlockContentBasic,
   BlockNotFound
 ]
