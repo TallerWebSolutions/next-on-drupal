bdecf0a48f74e7d6da2915a4179c0b5baf4c4947
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-03-19 00:42:04 -0300

    chore: added documentation deploy script

--- config/webpack/webpack.docz.js
+++ config/webpack/webpack.docz.js
@@ -1,10 +1,19 @@
 const path = require('path')
+const CreateFileWebpack = require('create-file-webpack')
 const common = require('./webpack.common')
 
 const cwd = process.cwd()
 
 module.exports = config => {
   config.resolve.alias['~docz'] = path.resolve(cwd, './docs/docz')
+
+  config.plugins.push(
+    new CreateFileWebpack({
+      path: config.output.path,
+      fileName: 'CNAME',
+      content: 'next-on-drupal.surge.sh'
+    })
+  )
 
   return common(config, {})
 }
