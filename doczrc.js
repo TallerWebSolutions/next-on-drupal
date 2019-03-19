efc8b22c26d0bfe0c2ee4ce6f4f1c8174c1fe49b
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-03-19 13:46:41 -0300

    docs: move menu configuration to doczrc

--- doczrc.js
+++ doczrc.js
@@ -2,6 +2,15 @@ const modifyBundlerConfig = require('./config/webpack/webpack.docz')
 
 module.exports = {
   title: 'Next ♥ Drupal',
+  menu: [
+    'Introduction',
+    'Getting Started',
+    'Environment & Commands',
+    'Structure',
+    'Composition',
+    'Routing',
+    'Layouts & Blocks'
+  ],
   wrapper: 'docs/docz/Provider',
   modifyBundlerConfig
 }
