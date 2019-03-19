e120213d7645bed6cacff319b23f36e0286d54f6
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-03-19 16:47:37 -0300

    chore: initiated storybook

---
+++ config/storybook/config.js
@@ -0,0 +1,8 @@
+import { configure } from '@storybook/react'
+
+// automatically import all files ending in *.stories.js
+const req = require.context('../../src', true, /.stories.js$/)
+
+const loadStories = () => req.keys().forEach(filename => req(filename))
+
+configure(loadStories, module)
