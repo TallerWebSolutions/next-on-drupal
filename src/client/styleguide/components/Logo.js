676bf6c758fb5e282d6f94377c055258cda4ae95
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-03-19 19:08:45 -0300

    docs: added Logo story

--- src/client/styleguide/components/Logo.js
+++ src/client/styleguide/components/Logo.js
@@ -1,3 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Image = styled.img`
+  height: 3em;
+`
+
 export default props => (
-  <img src='https://static.taller.work/logo.svg' { ...props } />
+  <Image src='https://static.taller.work/logo.svg' { ...props } />
 )
