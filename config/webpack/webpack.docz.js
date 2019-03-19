e101914706a18ff027d14b6aeb528f9945089454
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-03-18 22:22:27 -0300

    chore: removed unused package

--- config/webpack/webpack.docz.js
+++ config/webpack/webpack.docz.js
@@ -1,24 +1,10 @@
-const fs = require('fs')
 const path = require('path')
-const parse = require('parse-gitignore')
-const DirectoryTreePlugin = require('directory-tree-webpack-plugin')
 const common = require('./webpack.common')
 
-const ignores = parse(fs.readFileSync('.gitignore'))
 const cwd = process.cwd()
 
 module.exports = config => {
   config.resolve.alias['~docz'] = path.resolve(cwd, './docs/docz')
-  // prettier-ignore
-  config.resolve.alias['~source-tree'] = path.resolve(cwd, './.docz/source-tree.json')
-
-  config.plugins.push(
-    new DirectoryTreePlugin({
-      dir: './',
-      path: './.docz/source-tree.json',
-      exclude: ignores.map(ignore => new RegExp(`(^|/)${ignore}($|/)`))
-    })
-  )
 
   return common(config, {})
 }
