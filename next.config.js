335ad8f9c9e6f384e7be27d4212ec4a348b54adf
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-03-21 15:38:20 -0300

    chore: fixed production build dependencies

--- next.config.js
+++ next.config.js
@@ -1,37 +1,56 @@
 const withPlugins = require('next-compose-plugins')
-const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
-const webpack = require('./config/webpack/webpack.next')
 
-// webpack bundle analyzer.
-// @see: https://github.com/zeit/next-plugins/tree/master/packages/next-bundle-analyzer
-const analyzer = [
-  withBundleAnalyzer,
-  {
-    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-    bundleAnalyzerConfig: {
-      server: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/server.html'
-      },
-      browser: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/client.html'
-      }
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD
+} = require('next/constants')
+
+module.exports = (phase, info) => {
+  // core NextJS production only config.
+  const core = {
+    target: 'server',
+    distDir: 'build/client',
+    serverRuntimeConfig: {},
+    // available to both client and server-side.
+    env: {
+      GRAPHQL_HOST: process.env.GRAPHQL_HOST
     }
   }
-]
 
-// core NextJS config.
-const core = {
-  webpack,
-  target: 'server',
-  distDir: 'build/client',
-  serverRuntimeConfig: {},
-  // available to both client and server-side.
-  env: {
-    GRAPHQL_HOST: process.env.GRAPHQL_HOST
+  const plugins = [core]
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
+    const webpack = require('./config/webpack/webpack.next')
+
+    const builder = {
+      webpack
+    }
+
+    // webpack bundle analyzer.
+    // @see: https://github.com/zeit/next-plugins/tree/master/packages/next-bundle-analyzer
+    const analyzer = [
+      withBundleAnalyzer,
+      {
+        analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+        analyzeBrowser: ['browser', 'both'].includes(
+          process.env.BUNDLE_ANALYZE
+        ),
+        bundleAnalyzerConfig: {
+          server: {
+            analyzerMode: 'static',
+            reportFilename: '../bundles/server.html'
+          },
+          browser: {
+            analyzerMode: 'static',
+            reportFilename: '../bundles/client.html'
+          }
+        }
+      }
+    ]
+
+    plugins.unshift(analyzer, builder)
   }
-}
 
-module.exports = withPlugins([analyzer, core])
+  return withPlugins(plugins)(phase, info)
+}
